9b4d606f47a26e091f09b0fd12f9d1501f366a8d
Valentin Petrov <valpetrov.soccer@gmail.com>
2025-12-03 13:57:52 -0500

    Created the skeleton in Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -1,3 +1,37 @@
 #include "Checker.h"
 
+void checkUpperLeft(char2Dvector preGenGrid, char2Dvector& grid, int& x, int& y){
 
+}
+
+void checkUpperRight(char2Dvector preGenGrid, char2Dvector& grid, int& x, int& y){
+
+}
+
+void checkLowerLeft(char2Dvector preGenGrid, char2Dvector& grid, int& x, int& y){
+
+}
+
+void checkLowerRight(char2Dvector preGenGrid, char2Dvector& grid, int& x, int& y){
+
+}
+
+void checkLeft(char2Dvector preGenGrid, char2Dvector& grid, int& x, int& y){
+
+}
+
+void checkRight(char2Dvector preGenGrid, char2Dvector& grid, int& x, int& y){
+
+}
+
+void checkMiddle(char2Dvector preGenGrid, char2Dvector& grid, int& x, int& y){
+
+}
+
+void checkTop(char2Dvector vpreGenGrid, char2Dvector& grid, int& x, int& y){
+
+}
+
+void checkBottom(char2Dvector preGenGrid, char2Dvector& grid, int& x, int& y){
+
+}
