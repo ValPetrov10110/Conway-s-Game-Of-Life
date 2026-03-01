505cdb709b42b5185262682d73a0b62aeea18daf
ValPetrov10110 <valpetrov.soccer@gmail.com>
2026-02-28 20:14:23 -0500

    Project built successful

---
+++ build/CMakeFiles/GameOfLife.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/Valentin/Coding/GameOfLife/Checker.cpp" "CMakeFiles/GameOfLife.dir/Checker.cpp.o" "gcc" "CMakeFiles/GameOfLife.dir/Checker.cpp.o.d"
+  "/home/Valentin/Coding/GameOfLife/Input.cpp" "CMakeFiles/GameOfLife.dir/Input.cpp.o" "gcc" "CMakeFiles/GameOfLife.dir/Input.cpp.o.d"
+  "/home/Valentin/Coding/GameOfLife/LoadSave.cpp" "CMakeFiles/GameOfLife.dir/LoadSave.cpp.o" "gcc" "CMakeFiles/GameOfLife.dir/LoadSave.cpp.o.d"
+  "/home/Valentin/Coding/GameOfLife/Simulation.cpp" "CMakeFiles/GameOfLife.dir/Simulation.cpp.o" "gcc" "CMakeFiles/GameOfLife.dir/Simulation.cpp.o.d"
+  "/home/Valentin/Coding/GameOfLife/main.cpp" "CMakeFiles/GameOfLife.dir/main.cpp.o" "gcc" "CMakeFiles/GameOfLife.dir/main.cpp.o.d"
+  "" "GameOfLife" "gcc" "CMakeFiles/GameOfLife.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
